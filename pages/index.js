b6cc4ea46b420ac3f769305d17f9388c5326516a
Cyrus Duncan <cyrusd4264@gmail.com>
2020-07-01 19:28:14 -0600

    Inital Commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+function Home({products}) {
+  console.log(products);
+  React.useEffect(() => {
+    getProducts();
+  }, [])
+  return <>home</>;
+}
+async function getProducts() {
+  const url = 'http://localhost:3000/api/products'
+  const response = await axios.get(url);
+  console.log(response.data);
+}
+
+Home.getInitialProps = async () => {
+  // fetch data on the server
+  const url = 'http://localhost:3000/api/products'
+  const response = await axios.get(url);
+  // return response data as an object
+  return {products: response.data};
+  // note: This object will be merged with existing props
+}
+
+export default Home;
