d4c446f88d605b797356617f662d4fff11f5bc2b
Cyrus Duncan <cyrusd4264@gmail.com>
2020-07-02 22:27:56 -0600

    added delete and add product

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-     
+import mongoose from "mongoose";
+import shortid from "shortid";
+
 const { String, Number } = mongoose.Schema.Types;
- 
+
 const ProductSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,7 +14,8 @@ const ProductSchema = new mongoose.Schema({
   },
   sku: {
     type: String,
-    unique: true
+    unique: true,
+    default: shortid.generate()
   },
   description: {
     type: String,
@@ -24,7 +26,6 @@ const ProductSchema = new mongoose.Schema({
     required: true
   }
 });
-// export default mongoose.models.product || mongoose.model('Product', ProductSchema)
 
-export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
-
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
