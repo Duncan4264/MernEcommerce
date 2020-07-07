3f618c841a56d412d325c546ba3759641f83ada0
Cyrus Duncan <cyrusd4264@gmail.com>
2020-07-06 18:10:02 -0600

    Added errorhandling and cart page

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,14 @@
+import {Segment} from 'semantic-ui-react';
+import CartItemList from '../components/Cart/CartItemList';
+import CartSummery from '../components/Cart/CartSummary';
+
 function Cart() {
-  return <>cart</>;
+  return (
+    <Segment>
+      <CartItemList/>
+      <CartSummery/>
+    </Segment>
+  )
 }
 
 export default Cart;
